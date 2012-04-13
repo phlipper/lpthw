b639fd3b8de857b1ae711445de67d8920dde0633
Phil Cohen <github@phlippers.net>
2012-04-12 21:31:05 -0700

    example 3 extra credit

--- ex3.py
+++ ex3.py
@@ -1,23 +1,33 @@
+# print a string
 print "I will now count my chickens:"
 
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+# print the number of hens and roosters, numbers following labels
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
+# print a string
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# print a number
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
+# print a string
 print "Is it true that 3 + 2 < 5 - 7?"
 
-print 3 + 2 < 5 - 7
+# printa number
+print 3.0 + 2.0 < 5.0 - 7.0
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+# print the answers to the math questions, numbers following labels
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
 
+# print a string
 print "Oh, that's why it's False."
 
+# print a string
 print "How about some more."
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it less or equal?", 5 <= -2
+# print a boolean answer to the questions, answer following labels
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
