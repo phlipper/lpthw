83ad783fc749263d5a6b33a871d80fc531c53528
Phil Cohen <github@phlippers.net>
2012-04-12 21:25:46 -0700

    fix typo in example 2

--- ex2.py
+++ ex2.py
@@ -1,5 +1,5 @@
 # A comment, this is so you can read your program later.
-# Anything after this # is ignored by python
+# Anything after the # is ignored by python.
 
 print "I could have code like this." # and the comment after is ignored
 
