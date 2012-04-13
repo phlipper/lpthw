f05a5db1cf89488ef190a7f30c038a1263deefec
Phil Cohen <github@phlippers.net>
2012-04-12 22:08:52 -0700

    example 7 extra credit

--- ex7.py
+++ ex7.py
@@ -1,8 +1,12 @@
+# print out the beginning of the poem
 print "Mary had a little lamb."
 print "Its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
+
+# print 10 periods as a line separator
 print "." * 10  # what'd that do?
 
+# setup all the letters that spell "Cheese Burger"
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,5 +21,25 @@ end11 = "e"
 end12 = "r"
 
 # watch that comma at the end.  try removing it to see what happens
+# ^^ removing the comma at the end of the line prints "CheeseBurger" instead
+# of "Cheese Burger" ... no space between the words
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
+
+
+# Extra Credit
+# For these next few exercises, you will have the exact same extra credit.
+
+# Go back through and write a comment on what each line does.
+
+# Read each one backwards or out loud to find your errors.
+
+# From now on, when you make mistakes write down on a piece of paper what kind
+# of mistake you made.
+
+# When you go to the next exercise, look at the last mistakes you made and try
+# not to make them in this new one.
+
+# Remember that everyone makes mistakes. Programmers are like magicians who
+# like everyone to think they are perfect and never wrong, but it's all an act.
+# They make mistakes all the time.
