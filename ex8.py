42de1c704bf60b82a2ed4c60b4cd98e4a1e943d6
Phil Cohen <github@phlippers.net>
2012-04-12 22:11:13 -0700

    example 8 extra credit

--- ex8.py
+++ ex8.py
@@ -10,3 +10,15 @@ print formatter % (
     "But it didn't sing.",
     "So I said goodnight."
 )
+
+
+# Extra Credit
+
+# Do your checks of your work, write down your mistakes, try not to make them
+# on the next exercise.
+
+
+# Notice that the last line of output uses both single and double quotes for
+# individual pieces. Why do you think that is?
+
+# ^^ The output changes because of the single quote in the word `didn't`
